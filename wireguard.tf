e9e0738da26af32687675716cbfd1cd151bdecec
David Fry <david.fry@modusbox.com>
2021-09-07 10:14:15 +0100

    bump shared mod release

--- wireguard.tf
+++ wireguard.tf
@@ -60,7 +60,7 @@ resource "random_password" "wireguard_password" {
 }
 
 module "wireguard" {
-  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg?ref=v1.0.24"
+  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg?ref=v1.0.25"
 
   ami_id                  = var.use_latest_ami ? module.ubuntu-focal-ami.id : var.vpn_ami_list[var.region]
   instance_type           = var.vpn_instance_type
@@ -73,7 +73,7 @@ module "wireguard" {
 }
 
 /* module "wireguard_users" {
-  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg_user?ref=v1.0.24"
+  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg_user?ref=v1.0.25"
 
   dns_server        = "10.25.0.2"
   wireguard_address = module.wireguard.public_ip
