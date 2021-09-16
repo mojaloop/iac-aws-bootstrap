71489c9908510c9ef432ac93b0a9daee2f62c571
David Fry <david.fry@modusbox.com>
2021-09-16 19:07:42 +0100

    bump for wg fix

--- wireguard.tf
+++ wireguard.tf
@@ -51,7 +51,7 @@ resource "random_password" "wireguard_password" {
 }
 
 module "wireguard" {
-  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg?ref=v1.0.29"
+  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg?ref=v1.0.30"
 
   ami_id                  = var.use_latest_ami ? module.ubuntu-focal-ami.id : var.vpn_ami_list[var.region]
   instance_type           = var.vpn_instance_type
@@ -67,7 +67,7 @@ module "wireguard" {
 }
 
 /* module "wireguard_users" {
-  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg_user?ref=v1.0.29"
+  source = "git::https://github.com/mojaloop/iac-shared-modules.git//aws/wg_user?ref=v1.0.30"
 
   dns_server        = "10.25.0.2"
   wireguard_address = module.wireguard.public_ip
