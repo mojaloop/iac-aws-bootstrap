fc32a2d421491b617ee6b8f9662bc4c32a09bb15
David Fry <david.fry@modusbox.com>
2022-02-22 00:45:11 +0000

    fix ngw ref

--- modules/named-subnets/public.tf
+++ modules/named-subnets/public.tf
@@ -100,7 +100,7 @@ resource "aws_eip" "default" {
 resource "aws_nat_gateway" "default" {
   count         = local.ngw_count
   allocation_id = join("", aws_eip.default.*.id)
-  subnet_id     = aws_subnet.public[local.public_subnets[0]].id
+  subnet_id     = aws_subnet.public[keys(local.public_subnet_az_map)[0]].id
   tags          = module.public_label.tags
 
   lifecycle {
